ca20fef8bc8fd91fb65b5ff662d2f84f69130f26
robbyapriansyah979 <robbyapriansyah979@gmail.com>
2024-07-08 02:34:20 +0000

    belajar-flutter

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text('Belajar Flutter'),
+          centerTitle: true,
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
